d1d700317db962738b9029c4761c62d3a4d84023
hbayhan <talha.bayhan@partner.turktelekom.com.tr>
2026-04-14 11:38:25 +0300

    - added enable edge to edge feature

--- app/src/main/java/com/example/postapp/MainActivity.kt
+++ app/src/main/java/com/example/postapp/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.postapp
 
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -17,9 +22,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setWindowInsets()
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -28,5 +35,22 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         val appBarConfig = AppBarConfiguration(setOf(R.id.postListFragment))
         binding.toolbar.setupWithNavController(navController, appBarConfig)
+    }
+
+    private fun setWindowInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                binding.toolbar.setPadding(
+                    leftMargin, insets.top, rightMargin, bottomMargin
+                )
+                binding.root.setPadding(
+                    leftMargin, topMargin, rightMargin, insets.bottom
+                )
+            }
+
+            WindowInsetsCompat.CONSUMED
+        }
     }
 }
